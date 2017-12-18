dc7b8d1738da4459f1e993d48a128b0eeb717e14
Lars Heinemann <lhein.smx@gmail.com>
2017-12-18 23:09:18 +0100

    refactoring to minimize code duplication
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- syndesis/plugins/org.fusesource.ide.syndesis.extension.ui/src/org/fusesource/ide/syndesis/extensions/ui/wizards/SyndesisExtensionProjectWizard.java
+++ syndesis/plugins/org.fusesource.ide.syndesis.extension.ui/src/org/fusesource/ide/syndesis/extensions/ui/wizards/SyndesisExtensionProjectWizard.java
@@ -19,6 +19,8 @@ import org.eclipse.ui.IWorkbench;
 import org.fusesource.ide.syndesis.extensions.core.model.SyndesisExtension;
 import org.fusesource.ide.syndesis.extensions.ui.internal.Messages;
 import org.fusesource.ide.syndesis.extensions.ui.internal.SyndesisExtensionsUIActivator;
+import org.fusesource.ide.syndesis.extensions.ui.templates.BasicSyndesisExtensionXmlProjectTemplate;
+import org.fusesource.ide.syndesis.extensions.ui.util.NewSyndesisExtensionProjectMetaData;
 import org.fusesource.ide.syndesis.extensions.ui.wizards.pages.SyndesisExtensionProjectWizardExtensionDetailsPage;
 import org.fusesource.ide.syndesis.extensions.ui.wizards.pages.SyndesisExtensionProjectWizardLocationPage;
 import org.fusesource.ide.syndesis.extensions.ui.wizards.pages.SyndesisExtensionProjectWizardVersionsPage;
@@ -54,9 +56,9 @@ public class SyndesisExtensionProjectWizard extends Wizard implements INewWizard
 	 */
 	@Override
 	public boolean performFinish() {
-		final SyndesisExtension extension = getSyndesisExtension();
+		final NewSyndesisExtensionProjectMetaData metadata = getProjectMetaData();
 		try {
-			getContainer().run(false, true, new SyndesisExtensionProjectCreatorRunnable(locationPage.getProjectName(), locationPage.getLocationPath(), locationPage.isInWorkspace(), extension));
+			getContainer().run(false, true, new SyndesisExtensionProjectCreatorRunnable(metadata));
 		} catch (InterruptedException iex) {
 			SyndesisExtensionsUIActivator.pluginLog().logError("User canceled the wizard!", iex); //$NON-NLS-1$
 			Thread.currentThread().interrupt();
@@ -85,6 +87,17 @@ public class SyndesisExtensionProjectWizard extends Wizard implements INewWizard
 		addPage(extensionDetailsPage);
 	}
 
+	private NewSyndesisExtensionProjectMetaData getProjectMetaData() {
+		NewSyndesisExtensionProjectMetaData metadata = new NewSyndesisExtensionProjectMetaData();
+		metadata.setProjectName(locationPage.getProjectName());
+		if (!locationPage.isInWorkspace()) {
+			metadata.setLocationPath(locationPage.getLocationPath());
+		}
+		metadata.setSyndesisExtensionConfig(getSyndesisExtension());
+		metadata.setTemplate(new BasicSyndesisExtensionXmlProjectTemplate());
+		return metadata;
+	}
+	
 	private SyndesisExtension getSyndesisExtension() {
 		SyndesisExtension extension = new SyndesisExtension();
 		extension.setSpringBootVersion(versionPage.getSpringBootVersion());
