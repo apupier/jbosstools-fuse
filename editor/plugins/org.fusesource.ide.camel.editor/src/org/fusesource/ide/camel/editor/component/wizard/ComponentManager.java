64e6bc175c54e2255435366d1f23fe2157f67e12
Brian Fitzpatrick <bfitzpat@redhat.com>
2018-09-20 15:15:25 -0600

    FUSETOOLS-2869 - adding more dependencies via wsdl2rest wizard
    -- updating with the camel version from the project rather than trying
    to use the camel.version property
    -- updating so when user changes the Rest Configuration component, it
    adds the required dependency to the pom
    -- handling gathering dependencies thru the catalog rather than managing
    directly
    -- added progress wrapper to dependency work
    -- addressing next round of feedback and doing some cleanup
    -- updating wsdl2rest version to pick up latest changes
    -- updated to fix some reported sonar issues
    -- updating to 0.8.0.fuse-720034
    
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

--- editor/plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/component/wizard/ComponentManager.java
+++ editor/plugins/org.fusesource.ide.camel.editor/src/org/fusesource/ide/camel/editor/component/wizard/ComponentManager.java
@@ -13,6 +13,7 @@ package org.fusesource.ide.camel.editor.component.wizard;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -73,6 +74,17 @@ public class ComponentManager {
 
 	public Set<Component> getAllComponents() {
 		return new HashSet<>(model.getComponents());
+	}
+	
+	public Component getComponentById(String id) {
+		Iterator<Component> iter = model.getComponents().iterator();
+		while (iter.hasNext()) {
+			Component component = iter.next();
+			if (id.equalsIgnoreCase(component.getId())) {
+				return component;
+			}
+		}
+		return null;
 	}
 
 }
